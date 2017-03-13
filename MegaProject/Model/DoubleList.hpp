b79de31723c7d32e2bc855152fd30534e0a81d58
mattpow35 <mattpowley@Matts-MacBook-Pro.local>
2017-03-12 19:07:59 -0600

    Updated double list and queue from when I missed class

--- MegaProject/Model/DoubleList.hpp
+++ MegaProject/Model/DoubleList.hpp
@@ -14,7 +14,65 @@
 tempplate <class Type>
 class DoubleList : public DoublyLinkedList<Type>
 {
-    
+private:
+public:
+    DoubleList();
+    ~DoubleList();
+    void add(Type data);
+    Type remove(int index);
+    void addAtIndexFast(int index, Type value);
+    void addAtIndex(int index, Type value);
+    Type getFromIndex(int index);
+    Type getFromIndexFast(int index);
 };
+
+
+
+template <class Type>
+Type DoubleList<Type> :: getFromIndex(int index)
+{
+    
+}
+
+template <class Type>
+void DoubleList<Type> :: add(Type value)
+{
+    BiDirectionalNode<Type> * addedNode = new BiDirectionalNode<Type>(value);
+    if(this->getSize() == 0)
+    {
+        this->setFront(addedNode);
+        
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addedNode);
+        addNode->setPreviousPointer(this->getEnd());
+    }
+    this->setEnd(addedNode);
+    this->setSize(this->getSize() + 1);
+}
+
+template <class Type>
+Type DoubleList<Type> :: remove(int index)
+{
+    Type derp;
+    BiDirectionalNode<Type> * nodeToTakeOut = this->getFront();
+    for(int spot = 0; spot < index; spot++)
+    {
+        nodeToTakeOut = nodeToTakeOut->getNextPointer();
+    }
+    derp = nodeToTakeOut->getNodeData();
+    
+    BiDirectionalNode<Type> * prev = nodeToTakeOut->getPreviousPointer();
+    BiDirectionalNode<Type> * prev = nodeToTakeOut->getNextPointer();
+    
+    prev->setNextPointer(next);
+    next->setPreviousPointer(previous);
+    
+    delete nodeToTakeOut;
+    
+    this->setSize(this-getSize() - 1);
+    return derp;
+}
 
 #endif /* DoubleList_h */
--- MegaProject/Model/Queue.hpp
+++ MegaProject/Model/Queue.hpp
@@ -14,7 +14,122 @@
 template <class Type>
 class Queue : public DoublyLinkedList<Type>
 {
+private:
+public:
+    Queue();
+    ~Queue();
+    void add(Type data);
+    Type remove(int index);
+    void enqueue(Type data);
+    Type dequeue();
+    Type peek();
     
 };
-
+template <class Type>
+Queue<Type> :: Queue() : DoublyLinkedList<Type>()
+{
+    
+}
+//same as all other data structure destructors.
+template <class Type>
+Queue<Type> :: ~Queue()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove = this->getFront();
+    }
+}
+/*
+ Call the enqueue method.
+ Method implemented to ensure the child class is NOT abstract.
+ */
+template <class Type>
+void Queue<Type> :: add(Type value)
+{
+    enqueue(value);
+}
+/*
+ 1. Create node
+ 2. If size == 0, adjust fornt to point to new node
+ 3. else, add new node to ends next
+ 4. move end to new node
+ 5. adjust size.
+ */
+template <class Type>
+void Queue<Type> :: enqueue(Type insertedValue)
+{
+    BiDirectionalNode<Type>* added = new BiDirectionalNode<Type>(insertedValue);
+    
+    if(this->getSize() == 0 || this->getFront() == nullptr || this->getEnd() == nullptr)
+    {
+        this->setFront(added);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(added);
+        added->setPreviousPointer(this->getEnd());
+    }
+    this->setEnd(added);
+    this->setSize(this->getSize() + 1);
+}
+/*
+ Remove from Queue
+ check valid index and size
+ call dequeue
+ Implemented to avoid abstract status.
+ */
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index == 0 && this->getSize() > 0);
+    return dequeue();
+}
+/*
+ 1. assert size is valid
+ 2. get data from front node
+ 3. if size == 1, set end to nullptr
+ 4. else, move front to fronts next
+ 4a set fronts previous to nullptr.
+ 5. delete old fornt ndoe
+ 6. adjsut size down by 1
+ 7. return old value.
+ */
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    assert(this->getSize() > 0);
+    
+    Type removedValue = this->getFront()->getNodeData();
+    BiDirectionalNode<Type> * removeMe = this->getFront();
+    
+    if(this->getSize() ==1)
+    {
+        this->setEnd(nullptr);
+        this->setFront(nullptr);
+    }
+    else
+    {
+        this->setFront(removeMe->getNextPointer());
+    }
+    this->setFront()->setPreviousPointer(nullptr);
+    
+    delete removeMe;
+    this->setSize(this->getSize() - 1);
+    
+    return removedValue;
+}
+/*
+ 1. check that the size is greater than 0.
+ 2. Return the front objects data.
+ */
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->getSize() > 0);
+    
+    return this->getFront()->getNodeData();
+}
 #endif /* Queue_h */

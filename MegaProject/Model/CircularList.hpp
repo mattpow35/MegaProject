ff1dd32bfe543ebb37e66efe5a2eaf77e61216ba
mattpow35 <mpow2231@740s-pl-121.local>
2017-03-15 08:01:18 -0600

    made header for circle list and wrote the add method

--- MegaProject/Model/CircularList.hpp
+++ MegaProject/Model/CircularList.hpp
@@ -14,7 +14,54 @@
 template <class Type>
 class CircularList : public DoublyLinkedList<Type>
 {
-    
+private:
+public:
+    CircularList();
+    ~CircularList();
+    void add(Type data);
+    Type remove(int index);
+    Type getFromIndex(int index);
+    Type setAtIndex(int index, Type data);
 };
 
+template <class Type>
+CircularList<Type> :: CircularList() : DoublyLinkedList<Type>()
+{
+    
+}
+
+template  <class Type>
+CircularList<Type> :: ~CircularList()
+{
+    BiDirectionalNode<Type> * remove = this->getFront();
+    while(this->getFront() != nullptr)
+    {
+        this->setFront(this->getFront()->getNextPointer());
+        delete remove;
+        remove = this->getFront();
+    }
+}
+
+template <class Type>
+void CircularList<Type> :: add(Type data)
+{
+    BiDirectionalNode<Type> * addMe = new BiDirectionalNode<Type>(data);
+    
+    if(this->getSize() == 0)
+    {
+        this->setFront(addMe);
+        this->setEnd(addMe);
+        addMe->setPreviousPointer(this->getFront());
+        this->getFront()->setNextPointer(this->getEnd);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(addMe);
+        addMe->setPreviousPointer(this->getEnd());
+        addMe->setNextPointer(this->getFront());
+        this->getFront()->setPreviousPointer(addMe);
+        this->setEnd(addMe);
+    }
+    this->setSize(this->getSize() + 1);
+}
 #endif /* CircularList_h */

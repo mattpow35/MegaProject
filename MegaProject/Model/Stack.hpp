b9bb7765048acc6679c92c8a0fec3bc36bc9206d
mattpow35 <mpow2231@740s-pl-121.local>
2017-03-17 08:25:18 -0600

    updated pop method

--- MegaProject/Model/Stack.hpp
+++ MegaProject/Model/Stack.hpp
@@ -127,6 +127,10 @@ Type Stack<Type> :: pop()
     {
         update->setNextPointer(nullptr);
     }
+    else
+    {
+        this->setFront(nullptr);
+    }
     
     delete this->getEnd();
     

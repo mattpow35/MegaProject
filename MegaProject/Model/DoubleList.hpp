a9fe94e10a5ea3bd9c7a8740df075328cc1226f6
mattpow35 <mpow2231@740s-pl-121.local>
2017-03-17 07:45:10 -0600

    added the get at index and get at index fast method

--- MegaProject/Model/DoubleList.hpp
+++ MegaProject/Model/DoubleList.hpp
@@ -46,7 +46,44 @@ DoubleList<Type> :: ~DoubleList()
 template <class Type>
 Type DoubleList<Type> :: getFromIndex(int index)
 {
+    assert(index >= 0 && index < this->getSize());
+    Type valueAtIndex;
     
+    BiDirectionalNode<Type> * reference = this->getFront();
+    for(int position = 0; position < index; position++)
+    {
+        reference = reference->getNextPointer();
+    }
+    valueAtIndex = reference->getNodeData();
+    
+    return valueAtIndex;
+}
+
+template <class Type>
+Type DoubleList<Type> :: getFromIndexFast(int index)
+{
+    assert(index >= 0 && index < this->getSize());
+    Type valueAtIndex;
+    BiDirectionalNode<Type> * reference;
+    if(index < this->getSize() / 2)
+    {
+        reference = this->getFront();
+        for(int position = 0; position < index; position++)
+        {
+            reference = reference->getNextPointer();
+        }
+    }
+    else
+    {
+        reference = this->getEnd();
+        for(int position = this->getSize() - 1; position > index; position--)
+        {
+            reference = reference->getPreviousPointer();
+        }
+    }
+    
+    valueAtIndex = reference->getNodeData();
+    return valueAtIndex;
 }
 
 template <class Type>

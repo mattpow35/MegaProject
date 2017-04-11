b676c486e45cf40c0b6f8774258eec18d9fffbe3
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-11 08:45:36 -0600

    Implemented and finished the BinarySearchTreeNode class

--- MegaProject/Model/BinarySearchTreeNode.hpp
+++ MegaProject/Model/BinarySearchTreeNode.hpp
@@ -32,4 +32,61 @@ public:
     void setRightChild(BinarySearchTreeNode<Type> * right);
 };
 
+template <class Type>
+BinarySearchTreeNode<Type> :: BinarySearchTreeNode()
+{
+    this->root = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> :: BinarySearchTreeNode(Type data) : Node<Type>(data)
+{
+    this->root = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getRootPointer()
+{
+    return this->root;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getLeftChild()
+{
+    return this->leftChild;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getRightChild()
+{
+    return this->rightChild;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setRootPointer(BinarySearchTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setLeftChild(BinarySearchTreeNode<Type> * left)
+{
+    this->leftChild = left;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setRightChild(BinarySearchTreeNode<Type> * right)
+{
+    this->rightChild = right;
+}
+
+
+
+
+
+
 #endif /* BinarySearchTreeNode_h */

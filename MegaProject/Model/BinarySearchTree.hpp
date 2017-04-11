4290d0b2d1556bafda831328b04468e13b3fa185
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-11 09:23:55 -0600

    wrote the header section for the binary search tree class

---
+++ MegaProject/Model/BinarySearchTree.hpp
@@ -0,0 +1,38 @@
+//
+//  BinarySearchTree.hpp
+//  MegaProject
+//
+//  Created by Powley, Matthew on 4/11/17.
+//  Copyright © 2017 Powley, Matthew. All rights reserved.
+//
+
+#ifndef BinarySearchTree_h
+#define BinarySearchTree_h
+
+#include "Tree.hpp"
+#include "BinarySearchTreeNode.hpp"
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+private:
+    BinarySearchTreeNode<Type> * root;
+    int calculateSize(BinarySearchTreeNode<Type> * root);
+    void inOrderTraversal(BinarySearchTreeNode<Type> * inStart);
+    void preOrderTraversal(BinarySearchTreeNode<Type> * preStart);
+    void postOrderTraversal(BinarySearchTreeNode<Type> * postStart)
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinarySearchTreeNode<Type> * getRoot();
+    void setRoot(BinarySearchTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    
+}
+
+#endif /* BinarySearchTree_h */

e013c004b8433311b711ab6f9d8210bc4841d8a3
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-13 08:08:58 -0600

    added methods to header and wrote the insert method

--- MegaProject/Model/BinarySearchTree.hpp
+++ MegaProject/Model/BinarySearchTree.hpp
@@ -17,10 +17,13 @@ class BinarySearchTree : public Tree<Type>
 {
 private:
     BinarySearchTreeNode<Type> * root;
+    
     int calculateSize(BinarySearchTreeNode<Type> * root);
     void inOrderTraversal(BinarySearchTreeNode<Type> * inStart);
     void preOrderTraversal(BinarySearchTreeNode<Type> * preStart);
-    void postOrderTraversal(BinarySearchTreeNode<Type> * postStart)
+    void postOrderTraversal(BinarySearchTreeNode<Type> * postStart);
+    
+    void removeNode(BinarySearchTreeNode<Type> * & removable);
     
 public:
     BinarySearchTree();
@@ -29,9 +32,17 @@ public:
     BinarySearchTreeNode<Type> * getRoot();
     void setRoot(BinarySearchTreeNode<Type> * root);
     
+    
+    
     void inOrderTraversal();
     void preOrderTraversal();
     void postOrderTraversal();
+    
+    void printToFile();
+    
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type value);
     
 };
 
@@ -39,6 +50,12 @@ template <class Type>
 BinarySearchTree<Type> :: BinarySearchTree() : Tree<Type>()
 {
     this->root = nullptr;
+}
+
+template <class Type>
+BinarySearchTree<Type> :: ~BinarySearchTree()
+{
+    
 }
 
 template <class Type>
@@ -93,6 +110,50 @@ template <class Type>
 void BinarySearchTree<Type> :: postOrderTraversal(BinarySearchTreeNode<Type> * postStart)
 {
     
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: insert(Type itemToInsert)
+{
+    BinarySearchTreeNode<Type> * insertMe = new BinarySearchTreeNode<Type>(itemToInsert);
+    BinarySearchTreeNode<Type> * previous = nullptr;
+    BinarySearchTreeNode<Type> * current = root;
+    if (current == nullptr)
+    {
+        root = insertMe;
+    }
+    else
+    {
+        while(current != nullptr)
+        {
+            previous = current;
+            if(itemToInsert < current->getNodeData())
+            {
+                current = current->getLeftChild();
+            }
+            else if(itemToInsert > current->getNodeData())
+            {
+                current = current->getRightChild();
+            }
+            else//Remove cerr after verification of unddeststanding
+            {
+                cerr << "Item already exists - exiting" << endl;
+                delete insertMe;
+                return;
+            }
+        }
+        
+        if(previous->getNodeData() > itemToInsert)
+        {
+            previous->setLeftChild(insertMe);
+        }
+        else
+        {
+            previous->setRightChild(insertMe);
+        }
+        insertMe->setRoot(previous);
+        
+    }
 }
 
 

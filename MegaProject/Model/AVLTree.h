1015543c246a8fc2b4873ceb60a889b310b0a57a
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-21 08:04:58 -0600

    wrote the height difference method and the rotate left and right methods.

--- MegaProject/Model/AVLTree.h
+++ MegaProject/Model/AVLTree.h
@@ -23,8 +23,8 @@ private:
     
     BinarySearchTreeNode<Type> * balanceSubTree(BinarySearchTreeNode<Type> * parent);
     
-    BinarySearchTreeNode<Type> * insertNode(BinarySearchTreeNode<Type> * parent);
-    BinarySearchTreeNode<Type> * removeNode(BinarySearchTreeNode<Type> * parent);
+    BinarySearchTreeNode<Type> * insertNode(BinarySearchTreeNode<Type> * parent, Type value);
+    BinarySearchTreeNode<Type> * removeNode(BinarySearchTreeNode<Type> * parent, Type value);
     
     int heightDifference(BinarySearchTreeNode<Type> * parent);
     
@@ -36,5 +36,45 @@ public:
     void insert(Type itemToInsert);
     void remove(Type value);
 };
+
+/*
+ A negative value means the right is greater than the left
+ */
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinarySearchTreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = calculateHeight(node->getLeftChild());
+    int rightHeight = calculateHeight(node->getRightChild());
+    balance = leftHeight - rightHeight;
+    return balance;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: leftRotation(BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getLeftChild();
+    
+    parent->setLeftChild(changedNode->getRightChild());
+    
+    changedNode->setRightChild(parent);
+    
+    return changedNode;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: rightRotation(BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getRightChild();
+    
+    parent->setRightChild(changedNode->getLeftChild());
+    
+    changedNode->setLeftChild(parent);
+    
+    return changedNode;
+}
+ 
 
 #endif /* AVLTree_h */

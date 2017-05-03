56200d047b77ce5224defed502088f4a95d7da08
mattpow35 <mpow2231@740s-pl-121.local>
2017-05-03 09:14:37 -0600

    fixed typo in constructor

--- MegaProject/Model/HashTable.h
+++ MegaProject/Model/HashTable.h
@@ -44,7 +44,7 @@ HashTable<Type> :: HashTable()
     this->efficiencyPercentage = .667;
     this->size = 0;
     this->hashTableStorage = HashNode<Type> * [capacity];
-    std :: fill_n(hashTableStorage, updatedCapacity, nullptr);
+    std :: fill_n(hashTableStorage, capacity, nullptr);
 }
 
 template <class Type>
